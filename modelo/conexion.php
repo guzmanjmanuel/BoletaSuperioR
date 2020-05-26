6b29b0cb309f9a6d09c01cb6cc7b3558d041b03b
guzmanjmanuel <guzmanjmanuel@gmail.com>
2020-05-26 02:12:11 -0600

    Update conexion.php
    cambie la conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 
 
-		 $servidor="127.0.0.1";
-		 $usuario="root";
-		 $password="";
-		 $bd="boleta";
+		 $servidor="qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+		 $usuario="qp0p7jo1lv1xaf5k";
+		 $password="tdzjm1l9lkbny3q5";
+		 $bd="ol8fza01l7t2x85p";
 
 				$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
 		
